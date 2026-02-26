d2307df531e15cd290cc10c5f831a20f8fe0fbf8
abhaykoli1 <abhaykoli214@gmail.com>
2026-02-27 01:17:23 +0530

    C

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "http://187.77.185.244:8000";
 // export const API_URL = "https://qbwm3635-8000.inc1.devtunnels.ms";
 export const EditerApiKey = "mmhbthlhtzrh13w3l72uwx3clz7wezzvf8p63ohqejv5uiu4";
 // export const API_URL = "https://api.kalyanratan777.com";
